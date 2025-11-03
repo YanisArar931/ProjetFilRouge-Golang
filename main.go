5450563fefaafc9b3cd6c5338882e6ce18e0ec09
NicolasBELLANGER <nicobell93220@gmail.com>
2025-11-03 14:39:42 +0100

    feat: creation menu CRM

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("-----------------------------")
+	fmt.Println("Bienvenue dans notre Mini-CRM !")
+	fmt.Println("-----------------------------")
+	fmt.Println("1. Ajouter un contact")
+	fmt.Println("2. Lister tous les contacts")
+	fmt.Println("3. Supprimer un contact par son ID")
+	fmt.Println("4. Mettre à jour un contact")
+	fmt.Println("5. Quitter l'application")
+}
