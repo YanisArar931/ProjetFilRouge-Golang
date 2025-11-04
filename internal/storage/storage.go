1def560346707610f0f47b74efca0584a7076e9e
NicolasBELLANGER <nicobell93220@gmail.com>
2025-11-04 15:50:42 +0100

    feat: ajout des structs et interface dans storage

---
+++ internal/storage/storage.go
@@ -0,0 +1,24 @@
+package storage
+import "fmt"
+type Contact struct {
+	ID    int
+	Name  string
+	Email string
+}
+
+type ContactList struct {
+	Contacts map[int]*Contact
+	NextID   int
+}
+
+type Storer interface {
+	AddContact(contact *Contact) error
+	GetAllContacts() ([]*Contact, error)
+	GetByContactID(id int) (*Contact, error)
+	UpdateContact(id int, name, email string) error
+	DeleteContact(id int) error
+}
+
+var ErrorContactNotFound = func(id int) error {
+	return fmt.Errorf("contact avec l'ID %d non trouvé", id)
+}
